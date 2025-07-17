55c7c87256caac5f8ebd65058a873aadbd8fc0d2
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2025-07-17 16:48:40 +0700

    fix: db config

--- app/core/database.py
+++ app/core/database.py
@@ -1,13 +1,16 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
 
 # Dependency
 def get_db():

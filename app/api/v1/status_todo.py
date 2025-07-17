f5c78ccc531129908f27f28f64913e5cfc5cb35a
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2025-07-17 19:40:09 +0700

    fix: get response json

--- app/api/v1/status_todo.py
+++ app/api/v1/status_todo.py
@@ -10,26 +10,26 @@ router = APIRouter()
 @router.post("/status-todo")
 def create_status_todo(status_todo: StatusTodoCreate, db: Session = Depends(get_db)):
     result = status_todo_service.create_status_todo_service(db, status_todo)
-    return success_response(data=StatusTodoOut.from_orm(result).model_dump(), message="OK", status_code=201)
+    return success_response(data=StatusTodoOut.model_validate(result).model_dump(), message="OK", status_code=201)
 
 @router.get("/status-todo")
 def get_all_status_todo(db: Session = Depends(get_db)):
     status_todos = status_todo_service.get_all_status_todo_service(db)
-    return success_response(data=[StatusTodoOut.from_orm(t).model_dump() for t in status_todos])
+    return success_response(data=[StatusTodoOut.model_validate(t).model_dump() for t in status_todos])
 
 @router.get("/status-todo/{id}")
 def get_status_todo_by_id(id: int, db: Session = Depends(get_db)):
     status_todo = status_todo_service.get_status_todo_by_id_service(db, id)
     if not status_todo:
         return success_response(message="Not found", status_code=404)
-    return success_response(data=StatusTodoOut.from_orm(status_todo).model_dump())
+    return success_response(data=StatusTodoOut.model_validate(status_todo).model_dump())
 
 @router.put("/status-todo/{id}")
 def update_status_todo(id: int, data: StatusTodoCreate, db: Session = Depends(get_db)):
     result = status_todo_service.update_status_todo_service(db, id, data)
     if not result:
         return success_response(message="Not found", status_code=404)
-    return success_response(data=StatusTodoOut.from_orm(result).model_dump(), message="OK")
+    return success_response(data=StatusTodoOut.model_validate(result).model_dump(), message="OK")
 
 @router.delete("/status-todo/{id}")
 def delete_status_todo(id: int, db: Session = Depends(get_db)):
--- app/api/v1/todo.py
+++ app/api/v1/todo.py
@@ -10,26 +10,26 @@ router = APIRouter()
 @router.post("/todos")
 def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
     result = todo_service.create_todo_service(db, todo)
-    return success_response(data=TodoOut.from_orm(result).model_dump(), message="Created", status_code=201)
+    return success_response(data=TodoOut.model_validate(result).model_dump(), message="Created", status_code=201)
 
 @router.get("/todos")
 def get_all(db: Session = Depends(get_db)):
     todos = todo_service.get_all_todos_service(db)
-    return success_response(data=[TodoOut.from_orm(t).model_dump() for t in todos])
+    return success_response(data=[TodoOut.model_validate(t).model_dump() for t in todos])
 
 @router.get("/todos/{id}")
 def get_by_id(id: int, db: Session = Depends(get_db)):
     todo = todo_service.get_todo_by_id_service(db, id)
     if not todo:
         return not_found_response()
-    return success_response(data=TodoOut.from_orm(todo).model_dump())
+    return success_response(data=TodoOut.model_validate(todo).model_dump())
 
 @router.put("/todos/{id}")
 def update(id: int, data: TodoCreate, db: Session = Depends(get_db)):
     result = todo_service.update_todo_service(db, id, data)
     if not result:
         return success_response(message="Not found", status_code=404)
-    return success_response(data=TodoOut.from_orm(result).model_dump(), message="Updated")
+    return success_response(data=TodoOut.model_validate(result).model_dump(), message="Updated")
 
 @router.delete("/todos/{id}")
 def delete(id: int, db: Session = Depends(get_db)):
